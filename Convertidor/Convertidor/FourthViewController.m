2ce4dd92f2c31c5b198f4080e5e70b7f671b5445
Giovanny Suarez <suarezkrac@gmail.com>
2014-09-20 11:42:17 -0500

    nuevo
    nuvevo

--- Convertidor/Convertidor/FourthViewController.m
+++ Convertidor/Convertidor/FourthViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    conversor = [[Temperature alloc] init];
 }
 
 - (void)didReceiveMemoryWarning {
